93e7f34275685497c26e6ab41b15f5e37ff7d59d
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-20 17:30:38 +0100

    milestone 3 completed

--- js/script.js
+++ js/script.js
@@ -7,29 +7,42 @@ createApp({
         return {
 
             tasks: [
-            { 
-                text: "ciao",
-                done: false
-            },
-            {
-                text: "ciao",
-                done: true
-            },
-            {
-                text: "ciao",
-                done: true
-            }
-
+                { 
+                    text: "ciao",
+                    done: false
+                },
+                {
+                    text: "ciao",
+                    done: true
+                },
+                {
+                    text: "ciao",
+                    done: true
+                }
             ],
+
+            newTask: '',
 
         }
     },
     methods: {
 
-
         deleteTask(taskIndex) {
 
             this.tasks.splice(taskIndex, 1);
+
+        },
+
+        addTask() {
+
+            // aggiungo il testo scritto dentro il campo di input alla lista dei task
+            // testo del campo di input:
+            // console.log(this.newTask)
+
+            this.tasks.push({text: this.newTask, done: false}),
+
+            // cancello il contenuto del campo di input
+            this.newTask = "";
 
         },
 
