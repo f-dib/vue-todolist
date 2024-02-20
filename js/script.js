2f3b53787a18fda091a117dab536a3fdd50fca96
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-20 15:28:45 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// const creatApp = Vue.creatApp;
+// stessa cosa di scrivere: -->
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+
+            
+            
+        }
+    }
+}).mount('#app');
